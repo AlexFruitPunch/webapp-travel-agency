f81484a4714f11ec281197012aedf745577cf2c7
alexpagani <alexpaganibr@gmail.com>
2022-05-23 16:59:26 +0200

    Ho qualche problema a cambiare le dimensioni delle immagini ma ce l'ho quasi fatta

---
+++ Travel_Agency/Controllers/PacchettoViaggioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Travel_Agency.Database;
+using Travel_Agency.Models;
+
+namespace Travel_Agency.Controllers
+{
+    public class PacchettoViaggioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<PacchettoViaggio> Pacchettiviaggio = new List<PacchettoViaggio>();
+
+            using (TravelAgencyContext db = new TravelAgencyContext())
+            {
+                Pacchettiviaggio = db.PacchettiViaggio.ToList();
+            }
+                return View("Index", Pacchettiviaggio);
+        }
+    }
+}
