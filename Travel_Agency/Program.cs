7919ef2d53ec02a39d5bb1de8c9150a71c345fa4
alexpagani <alexpaganibr@gmail.com>
2022-05-24 12:51:31 +0200

    quasi terminato Milestone 2 ma ho degli errori sull'importanzione di dati

--- Travel_Agency/Program.cs
+++ Travel_Agency/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=PacchettoViaggio}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
